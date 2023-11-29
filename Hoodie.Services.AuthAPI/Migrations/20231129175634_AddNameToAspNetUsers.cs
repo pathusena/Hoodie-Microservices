4ed3d7da490c5aad319363ad5270d771e9970d81
Pathum Senaratna <pathusena@gmail.com>
2023-11-29 23:27:55 +0530

    AuthAPI Service changes

---
+++ Hoodie.Services.AuthAPI/Migrations/20231129175634_AddNameToAspNetUsers.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hoodie.Services.AuthAPI.Migrations
+{
+    public partial class AddNameToAspNetUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+        }
+    }
+}
--- Hoodie.Services.CouponAPI/Data/AppDbContext.cs
+++ Hoodie.Services.CouponAPI/Data/AppDbContext.cs
@@ -5,10 +5,7 @@ namespace Hoodie.Services.CouponAPI.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-            
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Coupon> Coupons { get; set; }
 
